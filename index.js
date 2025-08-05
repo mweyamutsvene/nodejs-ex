896cac7adb5f91724f27e7896be58aa07c72027c
Tommy Hanks <tommy.max.hanks@gmail.com>
2025-08-04 23:59:39 -0600

    chore: remove package-lock to match 69b8a1a3

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var app = express();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', (request, response) => {
+app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(PORT, () => {
-  console.log(`Node app is running on port ${PORT}`);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
